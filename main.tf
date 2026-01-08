3e3ddeffa3eeaa4f34c119f2bd00e8113cefd107
ismail <ismail.attar>
2026-01-08 14:53:46 +0000

    addded ec2 code

--- main.tf
+++ main.tf
@@ -62,3 +62,14 @@ module "ec2" {
   }]
 }
 
+
+resource "aws_instance" "sonar" {
+  ami = "ami-00ca570c1b6d79f36"
+  instance_type = "t3.small"
+  subnet_id = var.Public_subnet_Name
+
+  tags = {
+    Name = "VAPT_server"
+  }
+  
+}
